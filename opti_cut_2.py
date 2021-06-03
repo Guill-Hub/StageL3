bb8d655bad0265b1ca8a99a9258363d067511621
Guillaume Méroué <guillaume.meroue@ens-lyon.fr>
2021-06-03 15:29:26 +0200

    plotting

--- opti_cut_2.py
+++ opti_cut_2.py
@@ -2,6 +2,7 @@
 
 from math import factorial
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def subset_sum(n, k, target, memo=None):
@@ -36,7 +37,7 @@ def bobs_expected_utility(n, k, memo=None):
     return sum((total_utility - picked) * subset_sum(n, k, picked, memo)
                for picked in range(min_picked, max_picked + 1)) * factorial(k) * factorial(n - k) / factorial(n)
 
-
+        
 def optimal_egalitarian_cut(n):
     """
     Computes (and returns) the optimal cut for n object. In other words,
@@ -68,7 +69,6 @@ def optimal_utilitarian_cut(n):
     argmax = 0
     alice_max = total_utility
     bob_max = 0
-
     for k in range(1, n+1):
         alice = total_utility - int((n - k) * (n - k + 1) / 2)
         bob = bobs_expected_utility(n, k, memo)
@@ -79,10 +79,75 @@ def optimal_utilitarian_cut(n):
             bob_max = bob
             alice_max = alice
     return (argmax,alice_max,bob_max)
-        
+
+
+def optimal_nash_cut(n):
+    total_utility = int(n * (n + 1) / 2)
+    memo = -np.ones((n + 1, total_utility + 1, total_utility + 1), dtype=np.float)
+    prod_utility_max = int(n * (n + 1) / 2) # k = 0
+    argmax = 0
+    alice_max = total_utility
+    bob_max = 0
+    for k in range(1, n+1):
+        alice = total_utility - int((n - k) * (n - k + 1) / 2)
+        bob = bobs_expected_utility(n, k, memo)
+        prod_utility = alice * bob
+        if prod_utility > prod_utility_max:
+            prod_utility_max = prod_utility
+            argmax = k
+            bob_max = bob
+            alice_max = alice
+    return (argmax,alice_max,bob_max)       
+
+
+#####
+"""
+Au lieu de mettre les 3 je pourrai mettre F la fct d'aggreg en param puis faire F(x1,x2) à chaque fois ( en faire ne marche pas pour * )
+"""
+#####
 
 def test(n):
-    print(optimal_utilitarian_cut(n))
-    print(optimal_egalitarian_cut(n))
-    
-test(50)
+    u,e,n = optimal_utilitarian_cut(n),optimal_egalitarian_cut(n),optimal_nash_cut(n)
+    print(u,e,n)
+    return (u,e,n)
+
+def ploting(n):
+    U = []
+    E = []
+    N = []
+    X = []
+    for i in range(1,n):
+        U.append(optimal_utilitarian_cut(i)[0])
+        E.append(optimal_egalitarian_cut(i)[0])
+        N.append(optimal_nash_cut(i)[0])
+        X.append(i)
+    plt.plot(X,U)
+    plt.plot(X,E)
+    plt.plot(X,N)
+    plt.show()
+
+def ploting2(n):
+    A = []
+    B = []
+    X = []
+    for i in range(1,n):
+        o = optimal_egalitarian_cut(i)
+        A.append(o[1])
+        B.append(o[2])
+        X.append(i)
+    plt.plot(X,A, color = 'r')
+    plt.plot(X,B)
+    plt.show()
+
+def ploting3(n):
+    A = []
+    B = []
+    X = []
+    for i in range(2,n):
+        o = optimal_egalitarian_cut(i)
+        A.append(o[0]/i)
+        X.append(i)
+    plt.plot(X,A, color = 'r')
+    plt.show()
+
+ploting3(50)
